bf4f56654bbfb501c90e0ec7ae2f1150adb642f9
Pacatovisck <marcelovianadiaspacato@gmail.com.br>
2023-06-14 20:17:14 -0300

    feat: H2 Database, dev profile, JPA

--- src/main/java/com/pacatovisk/course/entities/User.java
+++ src/main/java/com/pacatovisk/course/entities/User.java
@@ -1,14 +1,20 @@
 package com.pacatovisk.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "TB_USER")
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
