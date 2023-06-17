a9f7e89f654b988daf5983a70a0f34d991887e67
Pacatovisck <marcelovianadiaspacato@gmail.com.br>
2023-06-17 09:17:23 -0300

    feat: Service layer, component registration

--- src/main/java/com/pacatovisk/course/resources/UserResource.java
+++ src/main/java/com/pacatovisk/course/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.pacatovisk.course.resources;
 
 import com.pacatovisk.course.entities.User;
+import com.pacatovisk.course.servicies.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        return ResponseEntity.ok().body( new User(1, "Marcelo", "marce", "32323232","32323232"));
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
---
+++ src/main/java/com/pacatovisk/course/servicies/UserService.java
@@ -0,0 +1,29 @@
+package com.pacatovisk.course.servicies;
+
+import com.pacatovisk.course.entities.User;
+import com.pacatovisk.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found with id " + id + "!"));
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+}
