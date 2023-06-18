bdc592d2080bbf529fad4ca8f99e32d57d744fe0
Pacatovisck <marcelovianadiaspacato@gmail.com.br>
2023-06-17 23:24:49 -0300

    feat: User Exception handling - delete

---
+++ src/main/java/com/pacatovisk/course/servicies/exceptions/DataBaseException.java
@@ -0,0 +1,9 @@
+package com.pacatovisk.course.servicies.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+    public DataBaseException(String msg) {
+        super(msg);
+    }
+
+}
